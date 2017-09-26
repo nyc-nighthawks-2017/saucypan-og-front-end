6e33e8418c62b561dce872f5184dd50cbbcfcf4f
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-09-26 17:11:44 -0400

    Form can update state based on multiple inputs

--- src/components/newRecipe.js
+++ src/components/newRecipe.js
@@ -16,21 +16,14 @@ class NewRecipe extends React.Component {
         directions: "",
         creator: ""
         }
-      }
-      this.handleInputChange = this.handleInputChange.bind(this);
+      };
+      // this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-  handleInputChange(event) {
-    event.preventDefault();
-    const { recipeName, category, difficultyLevel, ingredient, prepTime, directions, creator } = this.state.recipe;
-    const target = event.target;
-    const name = target.name;
-
-    this.setState({
-      recipe: {
-        recipeName: target.value
-      }
-    });
+  handleInputChange(name, event) {
+    const recipe = this.state.recipe;
+    recipe[name] = event.target.value
+    this.setState({recipe: recipe})
   }
 
   render() {
@@ -38,9 +31,9 @@ class NewRecipe extends React.Component {
       <div>
         <form method="post" action="https://stark-falls-42396.herokuapp.com/recipes">
           <label htmlFor="recipeName">Name:</label>
-          <input type="text" name="recipeName" value={this.state.recipeName} onChange={this.handleInputChange}></input>
+          <input type="text" name="recipeName" value={this.state.recipeName} onChange={this.handleInputChange.bind(this, 'recipeName')}></input>
           <label htmlFor="category">Category:</label>
-          <input type="text" name="category" list="categories" value={this.state.category}></input><br />
+          <input type="text" name="category" list="categories" value={this.state.category} onChange={this.handleInputChange.bind(this, 'category')}></input><br />
             <datalist id="categories">
               <option value="Appetizer"></option>
               <option value="Salad"></option>
@@ -48,7 +41,7 @@ class NewRecipe extends React.Component {
               <option value="Desserts"></option>
             </datalist>
           <label htmlFor="difficultyLevel">Difficulty Level:</label>
-            <select name="difficultyLevel" value ={this.state.difficultyLevel}>
+            <select name="difficultyLevel" value ={this.state.difficultyLevel} onChange={this.handleInputChange.bind(this, 'difficultyLevel')}>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -56,13 +49,13 @@ class NewRecipe extends React.Component {
               <option value="5">5</option>
             </select><br />
           <label>Add Ingredients:</label>
-            <textarea type="text" name="ingredient" value={this.state.ingredient}></textarea>
+            <textarea type="text" name="ingredient" value={this.state.ingredient} onChange={this.handleInputChange.bind(this, 'ingredient')}></textarea>
           <label htmlFor="prepTime">Prep Time (mins):</label>
-            <input type="text" name="prepTime" value={this.state.prepTime}></input><br />
+            <input type="text" name="prepTime" value={this.state.prepTime} onChange={this.handleInputChange.bind(this, 'prepTime')}></input><br />
           <label htmlFor="directions">Directions:</label>
-            <textarea name="directions" value={this.state.directions}></textarea>
+            <textarea name="directions" value={this.state.directions} onChange={this.handleInputChange.bind(this, 'directions')}></textarea>
           <label htmlFor="creator">Creator Name:</label>
-            <input type="text" name="creator" value={this.state.creator}></input>
+            <input type="text" name="creator" value={this.state.creator} onChange={this.handleInputChange.bind(this, 'creator')}></input>
           <input type="submit" value="Submit Recipe"></input>
         </form>
       </div>
