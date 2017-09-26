b406e869e588ee4d2f5f59a45e9072829794c46c
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-09-26 12:02:46 -0400

    Add form with name and category inputs

--- src/components/newRecipe.js
+++ src/components/newRecipe.js
@@ -3,7 +3,28 @@ import React from 'react';
 class NewRecipe extends React.Component {
   render() {
     return (
-      <h1>New Recipe</h1>
+      <div>
+        <form method="post" action="https://stark-falls-42396.herokuapp.com/recipes">
+          <label for="recipe[:name]">Name:</label>
+          <input type="text" name="recipe[:name]"></input>
+          <label for="recipe[:category]">Category:</label>
+          <input type="text" name="recipe[:category]" list="categories"></input><br />
+            <datalist id="categories">
+              <option value="Appetizer"></option>
+              <option value="Salad"></option>
+              <option value="Main"></option>
+              <option value="Desserts"></option>
+            </datalist>
+          <label for=""></label>
+          <input type="text" name=""></input>
+          <label for=""></label>
+          <input type="text" name=""></input>
+          <label for=""></label>
+          <input type="text" name=""></input>
+          <label for=""></label>
+          <input type="text" name=""></input>
+        </form>
+      </div>
     )
   }
 }
